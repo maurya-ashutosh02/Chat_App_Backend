ef5774b25095168720af13446d389ad4a280305d
Ashutosh Maurya <ashutoshmaurya9696@gmail.com>
2025-08-21 21:57:14 +0530

    Changed Cors

--- src/index.js
+++ src/index.js
@@ -11,12 +11,14 @@ import { app, server } from './lib/socket.js'
 dotenv.config();
 
 
-app.use(cors(
-    {
-        origin: "http://localhost:5173",
-        credentials: true,
-    }
-))
+app.use(cors({
+    origin: [
+        "http://localhost:5173",      
+        "https://chat-app-frontend-beta-five.vercel.app"
+    ],
+    credentials: true,
+}));
+
 
 app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
